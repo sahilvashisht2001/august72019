e75a0f179ea4f70f1e1236afb4b5c23ee27d0bf5
Sahil Vashisht <755857@student.tor.loc>
2019-08-07 17:14:26 -0400

    fourth commit

--- index3.js
+++ index3.js
@@ -1,12 +1,26 @@
-var express=require('express')
-var app=express()
+const express=require('express')
+const app=express()
+const bodyParser=require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
-    res.send('Hell World')
+    res.send('GET send')
 });
 
 app.post('/',function(req,res){
-    res.send('Hell World')
+    console.log(req.body);
+    res.send('POST send')
 });
+
+app.put('/',function(req,res){
+    res.send('PUT send')
+});
+
+app.delete('/',function(req,res){
+    res.send('DELETE send')
+});
+
 
 app.listen(3000);
